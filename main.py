7a5dece76f964c7958ffddc7c6c2cf195f7481ec
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-04 10:03:29 -0800

    created sidebar and selectbox for the classifiers

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 
 st.title("Machine Learning App")
+
+
+st.write("""
+         # Explore different classifier
+         Which one is the best
+         """)
+
+dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine dataset"))
+st.write(dataset_name)
