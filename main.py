a81d6e5d4130f1e55fc86cb55cd6ae8914f05a9f
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-04 10:24:30 -0800

    rendering shape of dataset and number of classes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from sklearn import datasets
+import numpy as np
 
 
 st.title("Machine Learning App")
@@ -10,4 +12,20 @@ st.write("""
          """)
 
 dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine dataset"))
-st.write(dataset_name)
+#st.write(dataset_name)
+classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
+
+def get_dataset(dataset_name):
+    if dataset_name == "Iris":
+        data = datasets.load_iris()
+    elif dataset_name == "Breast Cancer":
+        data = datasets.load_breast_cancer()
+    else:
+        data = datasets.load_wine()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write("shape of dataset", X.shape)
+st.write("number of classes", len(np.unique(y)))
